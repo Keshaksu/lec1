5b183db24c7235a690c18f7af6c5b838465878a6
862-19 <862-19@C41407>
2021-09-11 12:54:43 +0300

    Задание с bool

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,29 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            bool ispalindrom = true;
+            do
+            {
+                Console.WriteLine("Введите строку для проверки на палиндром");
+                var str =  Console.ReadLine();
+                for (int first = 0, last = str.Length - 1; first < last; ++first, --last)
+                {
+                    if (str[first] != str[last])
+                    {
+                        ispalindrom = false;
+                        break;
+                    }
+                }
+                Console.WriteLine(ispalindrom ? "Палиндром" : "Не палиндром");
+                Console.WriteLine("Проверить новую строку? Для продолжения нажмите (1)");
+                int q = Int32.Parse(Console.ReadLine());
+                if (q == 1) ispalindrom = true;
+                else ispalindrom = false;
+                Console.Clear();
+              }
+            while (ispalindrom == true);
+
+            Console.ReadLine();
         }
     }
 }
