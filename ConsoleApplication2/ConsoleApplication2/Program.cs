ed4e910c5a4a32b5182356a6cb813a36da8f6a0a
862-19 <862-19@C41407>
2021-09-11 13:57:15 +0300

    чтение_из_файла

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"C:\Users\862-19\Desktop\txt1.txt";
+            string[] readText = File.ReadAllLines(path);
+            foreach (string s in readText)
+            {
+                Console.Write(s);
+                bool ispalindrom = true;
+                for (int first = 0, last = s.Length - 1; first < last; ++first, --last)
+                {
+                    if (s[first] != s[last])
+                    {
+                        ispalindrom = false;
+                        break;
+                    }
+                }
+                if (ispalindrom)
+                    Console.WriteLine(" - Является палиндромом");
+                else Console.WriteLine(" - Не является палиндромом");
+            }
+        }
+    }
+}
